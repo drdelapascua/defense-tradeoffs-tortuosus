9860d42e6eb3b74ceb5e8de6e2b671da990c4f03
Danielle De La Pascua <drdelapascua@ucdavis.edu>
2024-02-13 22:19:53 -0800

    updated scripts

--- scripts/data_prep.R
+++ scripts/data_prep.R
@@ -7,6 +7,7 @@
 ### > libraries ----
 library(tidyr)
 library(dplyr)
+library(readxl)
 
 ### > load data ----
 
@@ -20,12 +21,28 @@ GSL$Location <- as.integer(GSL$Location)
 biomass <- read.csv("~/GitHub/defense-tradeoffs-tortuosus/data/mass.csv")
 colnames(biomass) <- c("Rack #", "Location", "Population", "mf", "treatment", "biomass")
 
+# site elevation, locality, and seed year data 
+
+pop_data <- read.csv("~/GitHub/defense-tradeoffs-tortuosus/data/elevation.csv")
+
 # join mass data & location data with GSL data
 
 data <- left_join(GSL, biomass)
 
-### > save data table ----
+data <- left_join(data, pop_data)
+
+### > prep data ----
+
+# filter so only the racks with D1-D6 are included
+to_keep <- c("D1", "D2", "D3", "D4", "D5", "D6")
+data = data %>%
+  filter(data$`Rack #` %in% to_keep)
+
+# filter dataset so only induced leaf is included
+d_induced = data %>%
+  filter(data$leaf_type == "induced")
+
+### > save big data table ----
 
 write.csv(data, "~/GitHub/defense-tradeoffs-tortuosus/data/dl.csv")
-
-
+write.csv(d_induced, "~/GitHub/defense-tradeoffs-tortuosus/data/dl-induced.csv")
--- scripts/data_visualization.R
+++ scripts/data_visualization.R
@@ -10,118 +10,111 @@ library(dplyr)
 library(ggplot2)
 
 ### > load data ----
-data <- read.csv("~/GitHub/defense-tradeoffs-tortuosus/data/dl.csv")
-
-### > prep data ----
-
-# filter so only the racks with D1-D6 are included
-to_keep <- c("D1", "D2", "D3", "D4", "D5", "D6")
-d = data %>%
-  filter(data$Rack.. %in% to_keep)
+data <- read.csv("~/GitHub/defense-tradeoffs-tortuosus/data/dl-induced.csv")
 
 ### > GSL compounds by population & treatment ----
 
 # 3MSO
 
-X3MSO_by_pop <- ggplot(data = d, aes(x = Population, y = X3MSO_5.2, fill = treatment)) + 
+X3MSO_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = X3MSO_5.2, fill = treatment)) + 
   geom_boxplot()
 
 X3MSO_by_pop
 
 # OH-Alkenyl
 
-OH.Alkenyl_by_pop <- ggplot(data = d, aes(x = Population, y = OH.Alkenyl_6, fill = treatment)) + 
+OH.Alkenyl_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = OH.Alkenyl_6, fill = treatment)) + 
   geom_boxplot()
 
 OH.Alkenyl_by_pop
 
 # 4MSO
 
-X4MSO_by_pop <- ggplot(data = d, aes(x = Population, y = X4MSO_7.1, fill = treatment)) + 
+X4MSO_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = X4MSO_7.1, fill = treatment)) + 
   geom_boxplot()
 
 X4MSO_by_pop
 
 # Allyl
 
-Allyl_by_pop <- ggplot(data = d, aes(x = Population, y = Allyl_7.4, fill = treatment)) + 
+Allyl_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = Allyl_7.4, fill = treatment)) + 
   geom_boxplot()
 
 Allyl_by_pop
 
 # 5MSO
 
-X5MSO_by_pop <- ggplot(data = d, aes(x = Population, y = X5MSO_10.2, fill = treatment)) + 
+X5MSO_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = X5MSO_10.2, fill = treatment)) + 
   geom_boxplot()
 
 X5MSO_by_pop
 
 # Butenyl
 
-Butenyl_by_pop <- ggplot(data = d, aes(x = Population, y = Butenyl_12.1, fill = treatment)) + 
+Butenyl_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = Butenyl_12.1, fill = treatment)) + 
   geom_boxplot()
 
-Butenyl_by_pop
+Butenyl_by_pop 
 
 # 3MT
 
-X3MT_by_pop <- ggplot(data = d, aes(x = Population, y = X3MT_13.6, fill = treatment)) + 
+X3MT_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = X3MT_13.6, fill = treatment)) + 
   geom_boxplot()
 
 X3MT_by_pop
 
 # MSOO
 
-MSOO_by_pop <- ggplot(data = d, aes(x = Population, y = MSOO_13.8, fill = treatment)) + 
+MSOO_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = MSOO_13.8, fill = treatment)) + 
   geom_boxplot()
 
 MSOO_by_pop
 
 # OH-I3M
 
-OHI3M_by_pop <- ggplot(data = d, aes(x = Population, y = OH.I3M_15.1, fill = treatment)) + 
+OHI3M_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = OH.I3M_15.1, fill = treatment)) + 
   geom_boxplot()
 
 OHI3M_by_pop
 
 # 4MT
 
-X4MT_by_pop <- ggplot(data = d, aes(x = Population, y = X4MT._15.5, fill = treatment)) + 
+X4MT_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = X4MT._15.5, fill = treatment)) + 
   geom_boxplot()
 
 X4MT_by_pop
 
 # Flavonol 16
 
-Flavonol16_by_pop <- ggplot(data = d, aes(x = Population, y = Flavonol_16.1, fill = treatment)) + 
+Flavonol16_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = Flavonol_16.1, fill = treatment)) + 
   geom_boxplot()
 
 Flavonol16_by_pop
 
 # I3M
 
-I3M_by_pop <- ggplot(data = d, aes(x = Population, y = I3M_16.7, fill = treatment)) + 
+I3M_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = I3M_16.7, fill = treatment)) + 
   geom_boxplot()
 
 I3M_by_pop
 
 # Flavonol 17
 
-Flavonol17_by_pop <- ggplot(data = d, aes(x = Population, y = Flavonol_17.5, fill = treatment)) + 
+Flavonol17_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = Flavonol_17.5, fill = treatment)) + 
   geom_boxplot()
 
 Flavonol17_by_pop
 
 # Flavonol 18
 
-Flavonol18_by_pop <- ggplot(data = d, aes(x = Population, y = Flavonol_18.5, fill = treatment)) + 
+Flavonol18_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = Flavonol_18.5, fill = treatment)) + 
   geom_boxplot()
 
 Flavonol18_by_pop
 
 # indole 
 
-Indole_by_pop <- ggplot(data = d, aes(x = Population, y =Indole_18.8, fill = treatment)) + 
+Indole_by_pop <- ggplot(data = data, aes(x = reorder(Population, Elevation), y = Indole_18.8, fill = treatment)) + 
   geom_boxplot()
 
 Indole_by_pop
@@ -134,33 +127,125 @@ Indole_by_pop
 
 # 3MSO
 
+growth_defense_3MSO <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = X3MSO_5.2)) + 
+  geom_point(aes(colour=Elevation)) + 
+  stat_smooth(method=lm)
+
+growth_defense_3MSO
+
 # OH-Alkenyl
+
+growth_defense_OHAlkenyl <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = OH.Alkenyl_6)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_OHAlkenyl
 
 # 4MSO
 
+growth_defense_4MSO <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = X4MSO_7.1)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_4MSO 
+
 # Allyl
+
+growth_defense_Allyl <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = Allyl_7.4)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_Allyl 
 
 # 5MSO
 
+growth_defense_5MSO <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = X5MSO_10.2)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_5MSO
+
 # Butenyl
+
+growth_defense_Butenyl <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = Butenyl_12.1)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_Butenyl
 
 # 3MT
 
+growth_defense_3MT <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = X3MT_13.6)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_3MT
+
+
 # MSOO
+
+growth_defense_MSOO <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = MSOO_13.8)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_MSOO
 
 # OH-I3M
 
+growth_defense_OH_I3M <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = OH.I3M_15.1)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_OH_I3M
+
 # 4MT
+
+growth_defense_4MT <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = X4MT._15.5)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_4MT
 
 # Flavonol 16
 
+growth_defense_Flavonol_16 <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = Flavonol_16.1)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_Flavonol_16
+
 # I3M
+
+growth_defense_Flavonol_I3M <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = I3M_16.7)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_Flavonol_I3M
 
 # Flavonol 17
 
+growth_defense_Flavonol_17 <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = Flavonol_17.5)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_Flavonol_17
+
 # Flavonol 18
+
+growth_defense_Flavonol_18 <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = Flavonol_18.5)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_Flavonol_18
 
 # indole 
 
+growth_defense_indole <- ggplot(subset(data, treatment %in% "C"), aes(x = biomass, y = Indole_18.8)) + 
+  geom_point(aes(colour=Elevation)) +
+  stat_smooth(method=lm)
+
+growth_defense_indole
+
 # > Multivariate analysis - PCA of compounds ----
+
 
